a5ba42e1e4fb9902e7a1625ecc7e3d4495c45660
quepas <kiepas.patryk@gmail.com>
2020-12-11 23:53:30 +0100

    Making the profiling demo a bit more interesting!

--- test/profiling.cpp
+++ test/profiling.cpp
@@ -13,13 +13,23 @@ using std::vector;
 int main() {
     string domain = "PAPI_TOT_INS";
     int overflow_sampling = 10000;
-    vector<string> events = {"L1D:REPLACEMENT", "PAPI_L1_ICM"};
-    peptalk::profiling::Init(domain, overflow_sampling, events, false);
+    vector<string> events = {"PAPI_SP_OPS"};
+    peptalk::profiling::Init(domain, overflow_sampling, events, true);
     peptalk::profiling::Start();
+    float acc = 1.0f;
+    // Should be exactly 10k single precision floating-point operations
+    for (int k = 0; k < 10000; ++k) {
+        acc = acc * -1.001f;
+    }
     peptalk::profiling::Stop();
-    auto measurement_1 = peptalk::profiling::GetProfile(0);
-    for (int i = 0; i < measurement_1.size(); ++i) {
-        cout << events[0] << " = " << measurement_1[i] << endl;
+    cout << "acc=" << acc << endl;
+    auto inst = peptalk::profiling::GetProfile(0);
+    auto fp_ops = peptalk::profiling::GetProfile(1);
+    auto address = peptalk::profiling::GetInstructionAddress();
+    for (int i = 0; i < fp_ops.size(); ++i) {
+        cout << domain << "=" << inst[i] << "; "
+             << events[0] << "=" << fp_ops[i] << "; @"
+             << address[i] << endl;
     }
     peptalk::profiling::Cleanup();
 }
